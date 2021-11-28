12f20aab4918245f77595c548ecebe4167e6e768
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-11-28 12:56:37 +0800

    Format function args position on enter

--- lua/pairs/default/pairs.lua
+++ lua/pairs/default/pairs.lua
@@ -107,8 +107,22 @@ M.parenthesis = {
       end
     end,
 
-    action = function()
-      return keys.enter .. keys.indent
+    action = function(pair)
+      -- find the position of opening parenthesis
+      local pos = vim.fn.searchpairpos('(', '', ')', 'bn')
+      local line, col = pos[1], pos[2]
+
+      local current_line = vim.api.nvim_win_get_cursor(0)[1]
+
+      local indent_level = current_line - line + 1
+      local indent = string.rep("  ", indent_level)
+
+      -- next line starts from the opening parenthesis col like in cindent
+      local extra_space = string.rep(" ", col - 1) .. indent
+
+      -- delete the whole line and add the space to accomodate
+      -- different indent styles across file types
+      return keys.enter .. keys.delete_line .. extra_space
     end
   },
 
--- lua/pairs/keys.lua
+++ lua/pairs/keys.lua
@@ -4,18 +4,19 @@ local function escape(s)
 end
 
 M = {
-  above     = escape('<C-o>O'),
-  backspace = escape('<bs>'),
-  enter     = escape('<cr>'),
-  tab       = escape('<tab>'),
-  space     = escape('<space>'),
-  delete    = escape('<del>'),
-  keep_undo = escape('<C-g>U'),
-  indent    = escape('<C-f>'),
-  up        = escape('<up>'),
-  down      = escape('<down>'),
-  left      = escape('<left>'),
-  right     = escape('<right>')
+  above       = escape('<C-o>O'),
+  backspace   = escape('<bs>'),
+  enter       = escape('<cr>'),
+  tab         = escape('<tab>'),
+  space       = escape('<space>'),
+  delete      = escape('<del>'),
+  delete_line = escape('<C-u>'),
+  keep_undo   = escape('<C-g>U'),
+  indent      = escape('<C-f>'),
+  up          = escape('<up>'),
+  down        = escape('<down>'),
+  left        = escape('<left>'),
+  right       = escape('<right>')
 }
 
 return M
