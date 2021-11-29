cf5358e7df4a2e3cfeef080e7a5f8635f1c851ae
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-11-29 09:21:28 +0800

    Add no complete condition to open fallback

--- lua/pairs/default/fallback.lua
+++ lua/pairs/default/fallback.lua
@@ -23,11 +23,28 @@ M.open = function(pair)
 
   -- if the left and right are the same chars
   -- then this condition provides the skip over action
-  if pair.left == pair.right and helper.get_right_char == pair.right then
-    return keys.right
-  else
-    return pair.left .. pair.right .. move_left
+  if pair.left == pair.right then
+    if helper.get_right_char() == pair.right then
+      return keys.right
+
+    elseif string.find(helper.get_left_char(), "[%w%p]") then
+      return pair.left
+
+    end
   end
+
+  -- if pair.left == pair.right and helper.get_right_char() == pair.right then
+  --   return keys.right
+
+  -- elseif pair.left == pair.right and string.find(helper.get_left_char(), "[%w%p]") then
+  --   return pair.left
+
+  -- else
+  --   return pair.left .. pair.right .. move_left
+  -- end
+
+  return pair.left .. pair.right .. move_left
+
 end
 
 return M
