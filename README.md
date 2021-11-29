c44e6078f0c9fc4ad0206cadd1e00da7444b87a1
Yuki Yoshimine <yuki07yuki@gmail.com>
2021-11-29 18:43:34 +0800

    Rename actions to mappigs

---
+++ README.md
@@ -0,0 +1,6 @@
+
+
+## Things to do
+
+- handle tags with multi characters eg. `<h1><\h1>`
+- should condition be removed from open and close actions?
--- lua/pairs/init.lua
+++ lua/pairs/init.lua
@@ -1,5 +1,5 @@
 local default = require 'pairs.default'
-local actions = require 'pairs.actions'
+local mappings = require 'pairs.mappings'
 
 local M = {}
 
@@ -8,7 +8,7 @@ function M.setup()
 
   _G.Pairs = {}
 
-  _G.Pairs.actions  = actions
+  _G.Pairs.mappings = mappings
   _G.Pairs.fallback = default.fallback
   _G.Pairs.pairs    = default.pairs
 
@@ -22,19 +22,19 @@ end
 function M.apply_mappings()
 
   for name, pair in pairs(Pairs.pairs) do
-    local rhs = ("v:lua.Pairs.actions.open(\"%s\")"):format(name)
+    local rhs = ("v:lua.Pairs.mappings.open(\"%s\")"):format(name)
     vim.api.nvim_set_keymap("i", pair.left, rhs, { expr = true, noremap = true } )
 
     if pair.close and pair.close.action then
-      local rhs = ("v:lua.Pairs.actions.close(\"%s\")"):format(name)
+      local rhs = ("v:lua.Pairs.mappings.close(\"%s\")"):format(name)
       vim.api.nvim_set_keymap("i", pair.right, rhs, { expr = true, noremap = true } )
       -- print(pair.close.key .. " --> " .. rhs)
     end
   end
 
-  vim.api.nvim_set_keymap("i", "<bs>", "v:lua.Pairs.actions.backspace()", { expr = true, noremap = true } )
-  vim.api.nvim_set_keymap("i", "<cr>", "v:lua.Pairs.actions.enter()", { expr = true, noremap = true } )
-  vim.api.nvim_set_keymap("i", " ", "v:lua.Pairs.actions.space()", { expr = true, noremap = true } )
+  vim.api.nvim_set_keymap("i", "<bs>", "v:lua.Pairs.mappings.backspace()", { expr = true, noremap = true } )
+  vim.api.nvim_set_keymap("i", "<cr>", "v:lua.Pairs.mappings.enter()", { expr = true, noremap = true } )
+  vim.api.nvim_set_keymap("i", " ", "v:lua.Pairs.mappings.space()", { expr = true, noremap = true } )
 
 end
 
